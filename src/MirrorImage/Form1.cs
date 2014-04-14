27178384cda3b86132f258453cafbf5ec3c82cdf
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-04-14 18:04:48 +0530

    Initial commit

---
+++ src/MirrorImage/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MirrorImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //read source image
+            Bitmap simg = new Bitmap("D:\\Image\\audrey.jpg");
+
+            //get source image dimension
+            int width = simg.Width;
+            int height = simg.Height;
+
+            //load source image in picturebox1
+            pictureBox1.Image = Image.FromFile("D:\\Image\\audrey.jpg");
+
+            //mirror image
+            Bitmap mimg = new Bitmap(width * 2, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int lx = 0, rx = width * 2 - 1; lx < width; lx++, rx--)
+                {
+                    //get source pixel value
+                    Color p = simg.GetPixel(lx, y);
+
+                    //set mirror pixel value
+                    mimg.SetPixel(lx, y, p);
+                    mimg.SetPixel(rx, y, p);
+                }
+            }
+
+            //load mirror image in picturebox2
+            pictureBox2.Image = mimg;
+
+            //save (write) mirror image
+            mimg.Save("D:\\Image\\MirrorImage.png");
+        }
+    }
+}
