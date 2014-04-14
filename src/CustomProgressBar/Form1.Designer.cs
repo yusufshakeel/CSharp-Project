b9e33c7156d3fdfb1fa53aa933ba4f9bae416f74
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-04-14 14:08:31 +0530

    Initial commit

---
+++ src/CustomProgressBar/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CustomProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picboxPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picboxPB
+            // 
+            this.picboxPB.Location = new System.Drawing.Point(12, 9);
+            this.picboxPB.Name = "picboxPB";
+            this.picboxPB.Size = new System.Drawing.Size(470, 50);
+            this.picboxPB.TabIndex = 0;
+            this.picboxPB.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 71);
+            this.Controls.Add(this.picboxPB);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Custom ProgressBar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picboxPB;
+    }
+}
+
