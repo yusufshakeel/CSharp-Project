73a2523ee4ec44f0ed2e752e894beafed34ccc18
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-04-14 14:08:37 +0530

    Initial commit

---
+++ src/CustomProgressBar/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomProgressBar
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        //pb = ProgressBar
+        double pbUnit;
+        int pbWIDTH, pbHEIGHT, pbComplete;
+
+        Bitmap bmp;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get picboxPB dimension
+            pbWIDTH = picboxPB.Width;
+            pbHEIGHT = picboxPB.Height;
+
+            pbUnit = pbWIDTH / 100.0;
+
+            //pbComplete - This is equal to work completed in % [min = 0 max = 100]
+            pbComplete = 0;
+
+            //create bitmap
+            bmp = new Bitmap(pbWIDTH, pbHEIGHT);
+
+            //timer
+            t.Interval = 50;    //in millisecond
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //graphics
+            g = Graphics.FromImage(bmp);
+
+            //clear graphics
+            g.Clear(Color.LightSkyBlue);
+
+            //draw progressbar
+            g.FillRectangle(Brushes.CornflowerBlue, new Rectangle(0, 0, (int)(pbComplete * pbUnit), pbHEIGHT));
+
+            //draw % complete
+            g.DrawString(pbComplete + "%", new Font("Arial", pbHEIGHT / 2), Brushes.Black, new PointF(pbWIDTH / 2 - pbHEIGHT, pbHEIGHT / 10));
+
+            //load bitmap in picturebox picboxPB
+            picboxPB.Image = bmp;
+
+            //update pbComplete
+            //Note!
+            //To keep things simple I am adding +1 to pbComplete every 50ms
+            //You can change this as per your requirement :)
+            pbComplete++;
+            if (pbComplete > 100)
+            {
+                //dispose
+                g.Dispose();
+                t.Stop();
+            }
+
+        }
+    }
+}
