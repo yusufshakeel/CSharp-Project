cab57c424e3c531b67f273408157cc8bd55cd9da
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-04-14 16:17:31 +0530

    Initial commit

---
+++ src/RandomPixelImage/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPixelImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width = 640, height = 320;
+
+            //bitmap
+            Bitmap bmp = new Bitmap(width, height);
+
+            //random number
+            Random rand = new Random();
+
+            //create random pixels
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //generate random ARGB value
+                    int a = rand.Next(256);
+                    int r = rand.Next(256);
+                    int g = rand.Next(256);
+                    int b = rand.Next(256);
+
+                    //set ARGB value
+                    bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+
+            //load bmp in picturebox1
+            pictureBox1.Image = bmp;
+
+            //save (write) random pixel image
+            bmp.Save("D:\\Image\\RandomImage.png");
+        }
+    }
+}
